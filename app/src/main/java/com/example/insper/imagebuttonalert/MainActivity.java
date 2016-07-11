c3c7e4da5f5bdb67d84b56173b6f4a84b7c79d84
Lucas Matheus Borges <lucasmatborges@gmail.com>
2016-07-10 22:20:09 -0300

    Teste de mudança

--- app/src/main/java/com/example/insper/imagebuttonalert/MainActivity.java
+++ app/src/main/java/com/example/insper/imagebuttonalert/MainActivity.java
@@ -27,7 +27,7 @@ public class MainActivity extends AppCompatActivity {
 
         Handler myhandler = new Handler();
         myhandler.postDelayed(runnable3secs, 3000); // 3000 = tempo de execução da splash ( 3000 milisegundos = 3 segundos)
-
+        //ff
     }
 
     public void nextActivity() {
